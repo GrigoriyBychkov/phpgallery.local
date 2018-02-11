6bda0d962c53bc82f44e20757651939467322c0f
Grigoriy <imansiberia@icloud.com>
2018-02-11 11:33:10 +0600

    update

--- controllers/imageController.php
+++ controllers/imageController.php
@@ -6,46 +6,50 @@
  * Time: 19:26
  */
 
-
-function uploadAction() {
-    $target_dir = "images/";
-    $uploadOk = 1;
-    $fileName = basename($_FILES["fileToUpload"]["name"]);
-    $imageFileType = strtolower(pathinfo($fileName,PATHINFO_EXTENSION));
+class ImageController
+{
+    function uploadAction()
+    {
+        $target_dir = "images/";
+        $uploadOk = 1;
+        $fileName = basename($_FILES["fileToUpload"]["name"]);
+        $imageFileType = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
-    if(isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-        if($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-        } else {
-            echo "File is not an image.";
+        if (isset($_POST["submit"])) {
+            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+            if ($check !== false) {
+                echo "File is an image - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            } else {
+                echo "File is not an image.";
+                $uploadOk = 0;
+            }
+        }
+// Check file size
+        if ($_FILES["fileToUpload"]["size"] > 5000000) {
+            echo "Sorry, your file is too large.";
             $uploadOk = 0;
         }
-    }
-// Check file size
-    if ($_FILES["fileToUpload"]["size"] > 5000000) {
-        echo "Sorry, your file is too large.";
-        $uploadOk = 0;
-    }
 // Allow certain file formats
-    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-        && $imageFileType != "gif" && $imageFileType != "jpe") {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        $uploadOk = 0;
-    }
+        if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif" && $imageFileType != "jpe"
+        ) {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }
 // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
-    } else {
-        $newId = Images::saveImageToDataBase($fileName, $imageFileType);
-        $target_file = $target_dir . "img_" . $newId . "." . $imageFileType;
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            echo '<script>window.location = "/"</script>';
-            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
         } else {
-            echo "Sorry, there was an error uploading your file.";
+            $newId = Images::saveImageToDataBase($fileName, $imageFileType);
+            $target_file = $target_dir . "img_" . $newId . "." . $imageFileType;
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                echo '<script>window.location = "/"</script>';
+                echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
         }
     }
 }
